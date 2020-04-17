0e674930cbc6cea9bc865dcf6d2a011cbb23c747
ITSMA <543654696@qq.com>
2020-04-17 14:48:20 +0800

    finish detail statusbar

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -2,5 +2,16 @@ Component({
   options: {
     addGlobalClass: true,
   },
-  data: {}
+  data: {
+    date: '2020-04-16',
+    year: '2020',
+    month: '05'
+
+  },
+  bindDateChange: function(e) {
+    console.log(e);
+    this.setData({
+        date: e.detail.value
+    })
+  }
 })
